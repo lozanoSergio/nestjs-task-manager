b642d38337b3088fc8d9e7e472b4a40eb0022f08
Sergio <sergylozanodelrio@gmail.com>
2020-12-22 16:39:42 +0100

    connect nestjs to database

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 15432,
+  username: 'postgres',
+  password: 'nestjstaskmanager',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
